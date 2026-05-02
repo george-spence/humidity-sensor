4918c1e4b01cc10e6c5b00930c7b0892e5bcc54d
gpspence <105173090+gpspence@users.noreply.github.com>
2026-05-02 14:34:19 +0100

    Remove Cloudflare ZTNA module

--- terraform/modules/cloudflare_ztna/imports.tf
+++
@@ -1,5 +0,0 @@
-# Import the existing WARP enrollment application into Terraform state.
-import {
-  to = cloudflare_zero_trust_access_application.warp
-  id = "${var.cloudflare_account_id}/${data.cloudflare_zero_trust_access_applications.warp.result[0].id}"
-}
